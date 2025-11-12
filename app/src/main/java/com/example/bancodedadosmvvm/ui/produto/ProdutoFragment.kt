aea06ae09a7176c747d2e4efb187db5977af62a1
Roberto Cirera Procópio Jr <35637328+rocpjunior@users.noreply.github.com>
2025-11-12 11:39:53 -0300

    implementacao de mais outros detalhes

--- app/src/main/java/com/example/bancodedadosmvvm/ui/produto/ProdutoFragment.kt
+++ app/src/main/java/com/example/bancodedadosmvvm/ui/produto/ProdutoFragment.kt
@@ -6,28 +6,52 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
 import com.example.bancodedadosmvvm.R
+import com.example.bancodedadosmvvm.ViewModelFactory
+import com.example.bancodedadosmvvm.model.Toto
+import com.example.bancodedadosmvvm.repository.TotoRepository
+import com.example.bancodedadosmvvm.repository.TotoRepositoryImplementacao
+import  com.example.bancodedadosmvvm.source.AppDatabase
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import android.app.AlertDialog
+import android.view.View.GONE
+import android.view.View.VISIBLE
+import android.widget.EditText
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
-class ProdutoFragment : Fragment() {
+class ProdutoFragment : Fragment(), TotoAdapter.AdaptandoLista {
 
     companion object {
         fun newInstance() = ProdutoFragment()
     }
 
-    private val viewModel: ProdutoViewModel by viewModels()
+    private lateinit var mainViewModel: ProdutoViewModel
+    private lateinit var adaptando: TotoAdapter
+    private lateinit var totoRepository: TotoRepository
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    lateinit var bancodedados: AppDatabase
 
-        // TODO: Use the ViewModel
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         return inflater.inflate(R.layout.fragment_produto, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        iniciarBanco()
+        totoRepositoryImpl =
+            TotoRepositoryImplementacao(
+            bancodedados.totoDao()
+        )
+        val factory = ViewModelFactory(totoRepositoryImplementacao)
+        mainViewModel = ViewModelProvider(this, factory).get(ProdutoViewModel::class.java)
     }
 
 }
--- app/src/main/java/com/example/bancodedadosmvvm/ui/produto/TotoAdapter.kt
+++ app/src/main/java/com/example/bancodedadosmvvm/ui/produto/TotoAdapter.kt
@@ -1,9 +1,11 @@
 package com.example.bancodedadosmvvm.ui.produto
 
 import android.content.Context
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.*
+import androidx.annotation.LayoutRes
 import androidx.core.content.ContextCompat
 import androidx.core.graphics.drawable.DrawableCompat
 import androidx.recyclerview.widget.RecyclerView
@@ -12,6 +14,72 @@ import com.example.bancodedadosmvvm.model.Toto
 
 class TotoAdapter (
     private val itemLista: MutableList<Toto>,
-    private val listando: AdapterListener,
+    private val listando: AdaptandoLista,
     private val contexto: Context
-)
+): RecyclerView.Adapter<TotoAdapter.ViewHolder>(){
+
+    override fun getItemCount() = itemLista.size
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int): ViewHolder {
+        return ViewHolder(parent.inflate(R.layout.item_a_ser_listado))
+    }
+
+    override fun onBindViewHolder(
+        holder: TotoAdapter.ViewHolder,
+        position: Int) {
+        holder.bindViewHolder(itemLista[position])
+    }
+
+    fun updateList(itemLista: MutableList<Toto>){
+        this.itemLista.clear()
+        this.itemLista.addAll(itemLista)
+        notifyDataSetChanged()
+    }
+
+    interface AdaptandoLista{
+        fun marcarComoConcluido(toto: Toto)
+        fun deletarItem(toto: Toto)
+    }
+
+    inner class ViewHolder(itemView: View):
+            RecyclerView.ViewHolder(itemView){
+                private lateinit var item: Toto
+            fun bindViewHolder(item: Toto){
+            this.item = item
+            val txtTarefa = itemView.findViewById<TextView>(R.id.txtTarefa)
+            txtTarefa.text = item.nome
+            configurarTotosCompletos(item)
+            val img = itemView.findViewById<ImageView>(R.id.imgCompletou)
+            img.setOnClickListener{listando.marcarComoConcluido(item)}
+                //Desenvoler um botão de remover
+
+                val elemento = itemView.findViewById<LinearLayout>(R.id.item_lista_controlador)
+                elemento.setOnClickListener {
+                    listando.deletarItem(item)
+                    return@setOnClickListener
+                }
+            }
+
+        private fun configurarTotosCompletos(toto: Toto){
+            if(toto.completou){
+                DrawableCompat.setTint(
+                    DrawableCompat.wrap(itemView.
+                    findViewById<ImageView>(R.id.imgCompletou).drawable),
+                    ContextCompat.getColor(contexto, R.color.black)
+                )
+            }
+            else{
+                DrawableCompat.setTint(
+                    DrawableCompat.wrap(itemView.
+                    findViewById<ImageView>(R.id.imgCompletou).drawable),
+                    ContextCompat.getColor(contexto, R.color.white)
+                )
+            }
+        }
+    }
+}
+fun ViewGroup.inflate(@LayoutRes layoutRes: Int, attachToRoot: Boolean = false): View{
+    return LayoutInflater.from(context).inflate(layoutRes, this, attachToRoot)
+}
